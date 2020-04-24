4f185d35cb245d635b35a75a06fafa27e13fe2e2
Daniel Zhang <dyzhang@ucsc.edu>
2020-04-23 22:32:07 -0700

    Initial Commit

---
+++ src/Score.js
@@ -0,0 +1,13 @@
+class Score extends Phaser.Scene{
+    constructor(){
+            super('scoreScene');
+    }
+
+    preload(){
+       //loading any assets necessary
+    }
+    create(){
+    }
+    update(){
+    }
+}
